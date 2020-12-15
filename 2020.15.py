320a71546008477b3a6bc0d134661cf72af99f5b
IntegralHamster <75609426+IntegralHamster@users.noreply.github.com>
2020-12-15 11:56:36 +0300

    Add files via upload

---
+++ 2020.15.py
@@ -0,0 +1,31 @@
+import re
+
+numbers = {}
+f = open ('input', 'r')
+inp = re.split(',', f.readline())
+for i in range(len(inp)):
+    numbers[int(inp[i])] = i + 1
+    inp[i] = int(inp[i])
+
+i = len(inp) - 1
+lastnumber = inp[-1]
+flag = 0
+
+while i < 30000000:
+    i += 1
+    if flag == 0:
+        nextnumber = 0
+    else:
+        nextnumber = i - numbers[lastnumber]
+    numbers[lastnumber] = i
+    if nextnumber in numbers.keys():
+        flag = 1
+    else:
+        flag = 0
+    lastnumber = nextnumber
+    if i == 30000000-1:
+        print(lastnumber)
+
+
+
+
