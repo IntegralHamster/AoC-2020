c99c986d82ca704e3985edbdf72bf1ab64e7434f
IntegralHamster <75609426+IntegralHamster@users.noreply.github.com>
2020-12-21 10:59:00 +0300

    Add files via upload

---
+++ 2020.21.py
@@ -0,0 +1,57 @@
+import re
+from hopcroftkarp import HopcroftKarp
+
+f = open ('input', 'r')
+ing = []
+allerg = []
+for line in f:
+    if line == "<>":
+        break
+    line = line.replace('(', '')
+    line = line.replace(')', '')
+    line = line.strip()
+    parse = re.split(' contains ', line)
+    ing.append(re.split(' ', parse[0]))
+    allerg.append(re.split(', ',parse[1]))
+
+correl = {}
+for i in range(len(ing)):
+    for j in ing[i]:
+        if j not in correl.keys():
+            correl[j] = allerg[i]
+        else:
+            tmp = correl[j].copy()
+            for k in allerg[i]:
+                if k not in tmp:
+                    tmp.append(k)
+            correl[j] = tmp
+
+for i in correl.keys():
+    tmp = correl[i].copy()
+    pop = []
+    for j in correl[i]:
+        for k in range(len(ing)):
+            if j in allerg[k] and i not in ing[k]:
+                pop.append(j)
+    for j in set(pop):
+        tmp.pop(tmp.index(j))
+    correl[i] = tmp
+
+matching = HopcroftKarp(correl).maximum_matching(keys_only=True)
+
+nonallerg = []
+for i in correl.keys():
+    if i not in matching.keys():
+        nonallerg.append(i)
+
+count = 0
+for i in ing:
+    for j in nonallerg:
+        if j in i:
+            count += 1
+print(count)
+
+print(matching)
+
+# eggs fish nuts peanuts sesame shellfish soy wheat
+# dtb,zgk,pxr,cqnl,xkclg,xtzh,jpnv,lsvlx
